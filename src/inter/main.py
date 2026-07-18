05f484a29dad827615dba569cafe35bd636798f8
unywhat <unywhat@gmail.com>
2026-07-18 18:29:52 +1000

    chore: 3d projection & focal length to the config was added. simple cube test works intended

--- src/inter/main.py
+++ src/inter/main.py
@@ -8,6 +8,29 @@ WIDTH, HEIGHT = config["general"]["window_width"], config["general"]["window_hei
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
+focal_length = config["general"]["focal_length"]
+
+def project(pos: tuple[float, float, float]):
+    x, y, z = pos
+    screen_x = WIDTH / 2 + focal_length * (x / z)
+    screen_y = HEIGHT / 2 - focal_length * (y / z)
+    return (screen_x, screen_y)
+
+
+camera_pos = (0, 0, -2)
+
+
+cube_points = [
+    (-1, -1, 3),
+    ( 1, -1, 3),
+    ( 1,  1, 3),
+    (-1,  1, 3),
+    (-1, -1, 5),
+    ( 1, -1, 5),
+    ( 1,  1, 5),
+    (-1,  1, 5),
+]
+
 def main():
     running = True
     while running:
@@ -15,6 +38,22 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
         screen.fill((0, 0, 0))
+
+        for point in cube_points:
+            x, y, z = point
+            xt, yt, zt = camera_pos
+            x -= xt
+            y -= yt
+            z -= zt
+            if z <= 0:
+                continue
+
+            pygame.draw.aacircle(
+                screen,
+                (255, 255, 255),
+                project((x, y, z)),
+                3
+            )
 
         pygame.display.flip()
         clock.tick(60)
